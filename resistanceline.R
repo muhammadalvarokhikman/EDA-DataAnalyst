55e36d572aa66c6a6edab1b9be0afcac30332ee8
Alvaro ~ Data Science <114148484+muhammadalvarokhikman@users.noreply.github.com>
2023-04-18 07:24:17 +0700

    Update resistanceline.R

--- resistanceline.R
+++ resistanceline.R
@@ -6,7 +6,10 @@
 library(ggplot2)
 library(dplyr)
 
+# import data dari excell terlebih dahulu,
+# nama data tergantung nama file
 # Membaca data
+
 
 # Membuat plot dengan ggplot2 dan menambahkan garis regresi
 ggplot(students_score, aes(x = students_score$Hours, y = students_score$Scores)) +
